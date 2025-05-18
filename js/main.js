bc427dfd64ccb9e0b1f5df8c1fe8e1f1e4d239af
Garieldas <dainiusrainys99@gmail.com>
2025-05-18 22:29:37 +0300

    Book constrcut method

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ openBtn.onclick = () => {
 closeBtn.onclick = () => {
     modal.style.display = 'none';
 }
+
+const myLibary = [];
+
+function Book(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
+
+function addBookToLibary(title, author, pages, status)
+{
+    const newBook = new Book(title,author,pages,status)
+    myLibary.push(newBook)
+    renderBook()
+}
+
+function renderBook() {
+    myLibary.forEach(book => {
+        console.log(book)
+    });
+}
